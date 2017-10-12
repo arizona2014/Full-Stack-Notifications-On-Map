6c2eeeb14954e18a7b84dca2d7c0e46d14ac7ce1
arizona2014 <andy.lisac@gmail.com>
2017-10-12 18:03:49 +0200

    modified dataservice files

---
+++ src/app/data.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class DataService {
+
+    constructor(public http: Http) { }
+
+    markers: any;
+
+    getMarkers(){
+
+        return this.http.get('http://localhost:3000/api/markers')
+            .map(result => this.markers = result.json());
+
+    }
+
+
+    saveMarker(newMarker: any){
+
+        let headers = new Headers({ 'Content-Type': 'application/json'});
+        let options = new RequestOptions({ headers: headers });
+        return this.http.post('http://localhost:3000/api/markers', JSON.stringify(newMarker), options)
+            .map(result => {
+                console.log(result)
+                this.markers = result.json();
+            });
+
+    }
+
+}
