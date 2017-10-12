c1808c9871f7f2bb7be53f8c439415bb6400d3ff
arizona2014 <andy.lisac@gmail.com>
2017-10-12 18:03:31 +0200

    modified app files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { MapComponent } from "./map/map.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+    title = 'app';
+    public _addingMarkers : boolean = false;
+    public _marketPlaced : boolean = false;
+    public _newMakerCoords : any = {};
+
+    changeFunctions(event) {
+        this._addingMarkers  = event.adding;
+        this._marketPlaced  = event.marker;
+        this._newMakerCoords = event.newMarkerCoords;
+    }
+
+    changeMap(event) {
+        this._addingMarkers  = event.adding;
+        this._marketPlaced  = event.marker;
+        this._newMakerCoords = event.newMarkerCoords;
+    }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { FunctionsComponent } from './functions/functions.component';
 import { AgmCoreModule } from "@agm/core";
-
+import { DataService } from "./data.service";
+import { HttpModule, Http } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -15,11 +15,12 @@ import { AgmCoreModule } from "@agm/core";
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     AgmCoreModule.forRoot({
       libraries: ["places"]
     })
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ DataService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
