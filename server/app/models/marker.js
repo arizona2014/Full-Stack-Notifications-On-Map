748dd359eb0fa775e1a035e50f1f1c4fa50a5ac9
arizona2014 <andy.lisac@gmail.com>
2017-10-16 17:48:56 +0200

    cleaned marker model

--- server/app/models/marker.js
+++ server/app/models/marker.js
@@ -3,10 +3,14 @@ var Schema       = mongoose.Schema;
 
 var MarkerSchema   = new Schema({
     label: String,
+    category: Number,
+    description: String,
     lat: Number,
     lng: Number,
+    created: Number,
     draggable: Boolean,
-    icon: String
+    icon: String,
+    status: String
 });
 
 module.exports = mongoose.model('Marker', MarkerSchema);
