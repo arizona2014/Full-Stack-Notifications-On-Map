06ce496e394ed28d669109770030a13d98060787
arizona2014 <andy.lisac@gmail.com>
2017-10-29 15:43:27 +0100

    app component prepared filtering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,17 +12,20 @@ export class AppComponent {
     public _addingMarkers : boolean = false;
     public _marketPlaced : boolean = false;
     public _newMakerCoords : any = {};
+    public _filteredMarkers : any = {};
 
     changeFunctions(event) {
         this._addingMarkers  = event.adding;
         this._marketPlaced  = event.marker;
         this._newMakerCoords = event.newMarkerCoords;
+        this._filteredMarkers = event.filteredMarkers;
     }
 
     changeMap(event) {
         this._addingMarkers  = event.adding;
         this._marketPlaced  = event.marker;
         this._newMakerCoords = event.newMarkerCoords;
+        this._filteredMarkers = event.filteredMarkers;
     }
 
 
